283038d257bd9f349f8a90b12c26c5555e48c154
yeon <dalcon10280@gmail.com>
2022-08-07 01:21:04 +0900

    test: Add Order entity methods

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -38,6 +39,7 @@ public class Order {
         member.getOrders().add(this);
     }
 
+    /* 연관 관계 메서드 */
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
@@ -46,5 +48,35 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    /* 생성 메서드 */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        Arrays.stream(orderItems).forEach(orderItem -> order.addOrderItem(orderItem));
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /* 비지니스 로직 */
+    // 주문 취소
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능 합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        orderItems.stream().forEach(OrderItem::cancel);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    /* 조회 로직 */
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        return orderItems.stream().parallel().mapToInt(OrderItem::getTotalPrice).sum();
     }
 }
