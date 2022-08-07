720952e7518403129b0d91aae476c3a1535e53dc
yeon <dalcon10280@gmail.com>
2022-08-07 13:57:50 +0900

    feat: Add OrderService, OrderRepository

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -6,11 +6,9 @@ import lombok.Setter;
 import javax.persistence.Entity;
 
 @Entity
-
 @Getter
 @Setter
 public class Book extends Item {
     private String author;
-
     private String isbn;
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "item")
-@Getter
+@Getter @Setter
 public abstract class Item {
 
     @Id @GeneratedValue
